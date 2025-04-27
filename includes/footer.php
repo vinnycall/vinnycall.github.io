c783a83111c58a9e83c1924f8258027eab8b24a2
vinnycall <vincentwijninga@gmail.com>
2025-04-27 14:35:30 +0200

    c

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-</main>
-    <footer class="container">
-        <p>&copy; <?php echo date("Y"); ?> [Friend's Name]. All rights reserved.</p>
-        <a class="nav-button" href="login.php">Admin</a>
-    </footer>
-</body>
-</html>
